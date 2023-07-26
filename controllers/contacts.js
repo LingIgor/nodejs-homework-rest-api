4bd392fd5585d67f088b05efc6a70572a794b704
Ling Igor <lingigor1990@gmail.com>
2023-07-26 22:23:28 +0300

    commit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,23 +1,6 @@
-// import express from "express";
-// import Joi from "joi";
-// import contactsService from "../../models/contacts.js";
 import { HttpError } from "../helpers/index.js";
 import Contact from "../models/contact.js";
 import { ctrlWrapper } from "../decorators/index.js";
-
-// const contactsRouter = express.Router();
-
-// const contactsAddSchema = Joi.object({
-//   name: Joi.string().required(),
-//   email: Joi.string().email().required().messages({
-//     "string.email": "Incorrect E-Mail Address",
-//     "any.required": "Email is required. Please provide an email address.",
-//   }),
-//   phone: Joi.string()
-//     .pattern(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/)
-//     .required(),
-//     favorite: Joi.boolean(),
-// });
 
 const getAll = async (req, res) => {
   const result = await Contact.find();
